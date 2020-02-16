263a5624f3878c9a03328a3edc8d7ed49100a316
Michael Kötter <michael.koetter@xtl-gmbh.de>
2020-02-16 10:43:29 +0100

    revert useless tweaks (not effective for TMC2208_STANDALONE)

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2239,15 +2239,15 @@
    */
   //#define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     150  // [mm/s] // config:CR10SPro
+  #define X_HYBRID_THRESHOLD     100  // [mm/s]
   #define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     150            // config:CR10SPro
+  #define Y_HYBRID_THRESHOLD     100
   #define Y2_HYBRID_THRESHOLD    100
-  #define Z_HYBRID_THRESHOLD      10            // config:CR10SPro
+  #define Z_HYBRID_THRESHOLD       3
   #define Z2_HYBRID_THRESHOLD      3
   #define Z3_HYBRID_THRESHOLD      3
   #define Z4_HYBRID_THRESHOLD      3
-  #define E0_HYBRID_THRESHOLD     50            // config:CR10SPro
+  #define E0_HYBRID_THRESHOLD     30
   #define E1_HYBRID_THRESHOLD     30
   #define E2_HYBRID_THRESHOLD     30
   #define E3_HYBRID_THRESHOLD     30
