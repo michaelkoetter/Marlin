69895298f0b033b919e5199cf0e84ca94b979c28
Michael Kötter <michael.koetter@xtl-gmbh.de>
2020-02-20 19:40:02 +0100

    disable FIX_MOUNTED_PROBE

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -878,7 +878,7 @@
  * A Fix-Mounted Probe either doesn't deploy or needs manual deployment.
  *   (e.g., an inductive probe or a nozzle-based probe-switch.)
  */
-#define FIX_MOUNTED_PROBE   // config:CR10SPro
+//#define FIX_MOUNTED_PROBE
 
 /**
  * Use the nozzle as the probe, as with a conductive
