18b6b8537854388e81a1d22f4cf1771b2dcf439c
Michael Kötter <michael@m-koetter.de>
2020-02-22 16:24:41 +0100

    fix E0 steps per unit

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -743,7 +743,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 140 }   // config:CR10SPro
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 133 }   // config:CR10SPro
 
 /**
  * Default Max Feed Rate (mm/s)
